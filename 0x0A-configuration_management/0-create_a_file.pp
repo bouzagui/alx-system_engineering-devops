16bdd2da633edb3b9a53cce3b8dff84478c6df65
bouzagui <badrbouzagui@gmail.com>
2024-06-29 03:22:53 +0100

    Done ✅

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
 # created file school in /tmp
 
 file { '/tmp/school':
-      mode => '0744',
-      owner => 'www-data',
-      group => 'www-data',
-      content => 'I love Puppet'
+  mode => '0744',
+  owner => 'www-data',
+  content => 'I love Puppet'
 }
