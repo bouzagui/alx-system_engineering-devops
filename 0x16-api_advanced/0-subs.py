abd63046eb9b77f267cea41a233a221f694854e1
bouzagui <badrbouzagui@gmail.com>
2024-08-13 13:29:56 +0100

    done ✅

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,7 +4,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ function that queries the Reddit API """
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {
         'User-Agent': 'custom-user-agent'
         }
