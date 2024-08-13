0abb06fead7ae0af0d441cc99cf81cd1a95341ee
bouzagui <badrbouzagui@gmail.com>
2024-08-13 14:09:52 +0100

    done ✅

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
-"""models doc"""
+"""Module to query the Reddit API and return the number of subscribers."""
+
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """ function that queries the Reddit API """
+    """Queries the Reddit API and returns the number of subscribers for a given subreddit.
+    
+    Args:
+        subreddit (str): The name of the subreddit to query.
+
+    Returns:
+        int: The number of subscribers, or 0 if the subreddit is invalid.
+    """
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {
-        'User-Agent': 'custom-user-agent'
-        }
+        'User-Agent': 'my-reddit-subscriber-counter'
+    }
     res = requests.get(url, headers=headers, allow_redirects=False)
     if res.status_code == 200:
         data = res.json()
-        return data['data'].get('subscribers')
+        return data['data'].get('subscribers', 0)
     else:
         return 0
