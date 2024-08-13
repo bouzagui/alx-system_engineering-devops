c341fe175fb966dc9f9614c0166c03e6e413c8bf
bouzagui <badrbouzagui@gmail.com>
2024-08-13 14:12:09 +0100

    done ✅

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"""Module to query the Reddit API and return the number of subscribers."""
+
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    """Queries the Reddit API and returns the number of subscribers for a given subreddit.
+    
+    Args:
+        subreddit (str): The name of the subreddit to query.
+
+    Returns:
+        int: The number of subscribers, or 0 if the subreddit is invalid.
+    """
+    url = 'https://www.reddit.com/r/{subreddit}/about.json'.format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        'User-Agent': 'my-reddit-subscriber-counter'
     }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code == 200:
+        data = res.json()
+        return data['data'].get('subscribers', 0)
+    else:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
