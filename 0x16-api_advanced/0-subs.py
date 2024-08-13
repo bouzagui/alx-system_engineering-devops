b6fde424422e02e8aec21b622ea85d59d7c141fb
bouzagui <badrbouzagui@gmail.com>
2024-08-13 13:32:25 +0100

    done ✅

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import requests
 
+url = f'https://www.reddit.com/r/{subreddit}/about.json'
+headers = {
+    'User-Agent': 'User_Agent_/3.0'
+    }
+
 
 def number_of_subscribers(subreddit):
     """ function that queries the Reddit API """
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {
-        'User-Agent': 'custom-user-agent'
-        }
     res = requests.get(url, headers=headers, allow_redirects=False)
     if res.status_code == 200:
         data = res.json()
