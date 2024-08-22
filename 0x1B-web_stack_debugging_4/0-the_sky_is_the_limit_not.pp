bb3119c9b59ca81523e3ad3fa332e9b6027ec5c1
bouzagui <badrbouzagui@gmail.com>
2024-08-22 17:54:33 +0100

    done ✅

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,9 @@
 exec { 'fix--for-nginx':
   command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
-  path    => 'shell',
+  path    => '/usr/local/bin/:/bin/',
 }
 
 exec { 'nginx-restart':
   command => 'service nginx restart',
-  path    => 'shell',
+  path    => '/usr/sbin/:/sbin/:/usr/local/bin/:/bin/',
 }
